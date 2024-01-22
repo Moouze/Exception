f8f992e286f460d72b99a858ed3744ffc764eb12
Lucas <lucas_dev@outlook.com.br>
2024-01-22 13:56:11 -0300

    implementação da classe conta

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,32 @@
 package application;
 
+import entities.Account;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Digite os dados da conta bancária: ");
+        System.out.print("Numero: ");
+        int numero = sc.nextInt();
+        System.out.print("Titular: ");
+        sc.nextLine();
+        String titular = sc.nextLine();
+        System.out.print("Saldo: ");
+        Double saldo = sc.nextDouble();
+        System.out.print("Limite de saque: ");
+        Double limiteSaque = sc.nextDouble();
+
+        Account conta = new Account(numero, titular, saldo, limiteSaque);
+
+        System.out.println();
+        System.out.println("Informe o valor do deposito: ");
+        double quantia = sc.nextDouble();
+        conta.deposito(quantia);
+        System.out.printf("Saldo total: %.2f", conta.getSaldo());
     }
 }
